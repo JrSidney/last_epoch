f304b884d852f6ba0c8e1ec5266f26dd911f604c
JrSidney <jrsistemas@gmail.com>
2025-04-23 09:07:08 -0300

    Atualizando arquivos

---
+++ UpdateFolder.bat
@@ -0,0 +1,3 @@
+cd C:\Users\jrsid\AppData\LocalLow\Eleventh Hour Games\Last Epoch\Filters
+git fetch origin
+git checkout main --force
---
+++ UpdateRepo.bat
@@ -0,0 +1,4 @@
+cd C:\Users\jrsid\AppData\LocalLow\Eleventh Hour Games\Last Epoch\Filters
+git add .
+git commit -m "Atualizando arquivos"
+git push origin main
